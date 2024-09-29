0b6a84f8e155bfc00ecd0b9484837f2e3cf02417
ASHISH KUMAR BHOI <128516103+ashish789bhoi@users.noreply.github.com>
2024-09-30 03:32:48 +0530

    Root files added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const ejsMate = require("ejs-mate");
+
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engines
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Views'));
+app.engine('ejs', ejsMate); 
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+   res.render("login");
+})
+
+app.get("/dashboard",(req,res)=>{
+  res.render("dashboard");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
